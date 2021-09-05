4c65be474eb25012dbf18f5b9de716a0a720a3e0
Vibin <vibin.cr22@gmail.com>
2021-09-05 08:28:29 +0530

    WorkerDetails updated with profile picture upload

--- app/src/main/java/com/killerinstinct/hobsapp/WorkerDetails.kt
+++ app/src/main/java/com/killerinstinct/hobsapp/WorkerDetails.kt
@@ -1,21 +1,30 @@
 package com.killerinstinct.hobsapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.view.children
+import androidx.core.widget.addTextChangedListener
 import com.killerinstinct.hobsapp.databinding.ActivityWorkerDetailsBinding
 import com.google.android.material.chip.Chip
 import com.killerinstinct.hobsapp.viewmodel.WorkerDetailsViewModel
+import com.killerinstinct.hobsapp.worker.WorkerMainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class WorkerDetails : AppCompatActivity() {
 
     lateinit var binding: ActivityWorkerDetailsBinding
     private val viewModel: WorkerDetailsViewModel by viewModels()
+    private var imageUri: Uri? = null
 
     private val categories = listOf(
         "Plumber",
@@ -42,27 +51,61 @@ class WorkerDetails : AppCompatActivity() {
         }
 
         binding.btnOk.setOnClickListener {
-            val list = binding.categoryCg.
-                    children.toList().filter {
+            val list = binding.categoryCg.children.toList().filter {
                 (it as Chip).isChecked
             }
             val cats = mutableListOf<String>()
-            for (chip in list){
+            for (chip in list) {
                 val str = (chip as Chip).text
                 cats.add(str.toString())
             }
 
-            val state = viewModel.updateWorkerInfo(
-                binding.wdLocation.text.toString(),
-                binding.wdPhonenumber.text.toString(),
-                binding.wdExperience.text.toString(),
-                binding.wdMinwage.text.toString(),
-                cats
-            )
+            CoroutineScope(Dispatchers.IO).launch {
+                viewModel.updateWorkerInfo(
+                    binding.wdLocation.text.toString(),
+                    binding.wdPhonenumber.text.toString(),
+                    binding.wdExperience.text.toString(),
+                    binding.wdMinwage.text.toString(),
+                    cats,
+                    imageUri
+                ){
+                    if (it) {
+                        Toast.makeText(this@WorkerDetails, "Details updated", Toast.LENGTH_SHORT)
+                            .show()
+                        startActivity(Intent(this@WorkerDetails, WorkerMainActivity::class.java))
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this@WorkerDetails, "Error", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
 
-//            Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.addPropic.setOnClickListener {
+            fetchImage()
         }
 
 
+
+    }
+
+    private fun fetchImage() {
+        val intent = Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+        }
+        startActivityForResult(intent, 100)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && data != null) {
+            imageUri = data.data
+            binding.ivPropic.setImageURI(imageUri)
+        }
     }
 }
--- app/src/main/java/com/killerinstinct/hobsapp/viewmodel/WorkerDetailsViewModel.kt
+++ app/src/main/java/com/killerinstinct/hobsapp/viewmodel/WorkerDetailsViewModel.kt
@@ -1,16 +1,19 @@
 package com.killerinstinct.hobsapp.viewmodel
 
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.launch
 
-class WorkerDetailsViewModel: ViewModel() {
+class WorkerDetailsViewModel : ViewModel() {
 
     private val userUid = FirebaseAuth.getInstance().currentUser?.uid
     private val db = FirebaseFirestore.getInstance()
+    private val storage = FirebaseStorage.getInstance()
     private val TAG = "WorkerDetailsViewModel"
 
     fun updateWorkerInfo(
@@ -18,8 +21,10 @@ class WorkerDetailsViewModel: ViewModel() {
         phoneNumber: String,
         experience: String,
         minWage: String,
-        category: List<String>
-    ): Boolean {
+        category: List<String>,
+        uri: Uri?,
+        isCompleted: (Boolean) -> Unit
+    ) {
         val workerData = mapOf(
             "location" to location,
             "phoneNumber" to phoneNumber,
@@ -35,23 +40,51 @@ class WorkerDetailsViewModel: ViewModel() {
                         Log.d(TAG, "updateWorkerInfo: successfully added")
                         db.collection("Worker")
                             .document(userUid)
-                            .update("category",category)
+                            .update("category", category)
                             .addOnSuccessListener {
                                 Log.d(TAG, "updateWorkerInfo: added category")
+                                uploadProPic(uri,userUid) //Upload Profile pic
+                                isCompleted(true)
                             }.addOnFailureListener {
+                                isCompleted(false)
                                 Log.d(TAG, "updateWorkerInfo: category not added")
                             }
                     }.addOnFailureListener {
+                        isCompleted(false)
                         Log.d(TAG, "updateWorkerInfo: WorkerInfo not added")
                     }
             }
-            return true
-        }
-        else
-        {
-            return false
+        } else {
+            isCompleted(false)
         }
     }
 
+    fun uploadProPic(uri: Uri?,userUid: String){
+        viewModelScope.launch {
+            if (uri == null)
+                return@launch
+            val ref = storage.getReference("$userUid/profilePicture")
+            ref.putFile(uri)
+                .addOnSuccessListener {
+                    Log.d(TAG, "uploadImage: Success")
+                    ref.downloadUrl.addOnSuccessListener {
+                        Log.d(TAG, "download url success")
+                        db.collection("Worker")
+                            .document(userUid)
+                            .update("profilePic", it.toString())
+                            .addOnSuccessListener {
+                                Log.d(TAG, "image url stored in db successfully")
+                            }.addOnFailureListener {
+                                Log.d(TAG, "image url failed to store in db")
+                            }
+                    }.addOnFailureListener {
+                        Log.d(TAG, "updateWorkerInfo: $it")
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "updateWorkerInfo: $it")
+                }
+        }
+    }
 
 }
